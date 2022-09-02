ec644160b111783e1046b50f449e4c2b322433d3
KOVU9529 <bapgiordano@gmail.com>
2022-09-02 19:39:06 +0200

    struttura creata

---
+++ laravel-boolpress/resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+@section('content')
+    <h1>Benvenuto nella sezione privata</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+       Quae, libero! Voluptas fugiat repellendus quisquam rem, totam excepturi magnam deleniti labore architecto, 
+       blanditiis distinctio nostrum, porro illum amet. Nobis, dicta a.</p>
+@endsection
