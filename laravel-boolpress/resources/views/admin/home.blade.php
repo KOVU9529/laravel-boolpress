0d58a4428c1697472c0766d53a14b208a63d3a13
KOVU9529 <bapgiordano@gmail.com>
2022-09-05 10:33:31 +0200

    stampa in pagina nome e e-mail utente dopo in login

--- laravel-boolpress/resources/views/admin/home.blade.php
+++ laravel-boolpress/resources/views/admin/home.blade.php
@@ -4,4 +4,6 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
        Quae, libero! Voluptas fugiat repellendus quisquam rem, totam excepturi magnam deleniti labore architecto, 
        blanditiis distinctio nostrum, porro illum amet. Nobis, dicta a.</p>
+    <h2>Name: {{$user->name}}</h2>
+    <h3>E-mail: {{$user->email}}</h3>
 @endsection
